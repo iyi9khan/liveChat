e6eacedb35b8dabd630037b021e543dfce408918
iyi9khan <iyi9khan@gmail.com>
2025-06-24 16:12:00 +0600

    check

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
-import Registration from "./pages/Registration";
+// import { createBrowserRouter, RouterProvider } from "react-router";
+// import Registration from "./pages/Registration";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/registration",
-      element: <Registration />,
-    },
-  ],
-  { 
-    basename: "/liveChat/" // Correct property name is 'basename'
-  }
-);
+// const router = createBrowserRouter(
+//   [
+//     {
+//       path: "/registration",
+//       element: <Registration />,
+//     },
+//   ],
+//   {
+//     basename: "/liveChat/"
+//   }
+// );
+
+// function App() {
+//   return (
+//     <>
+//       <RouterProvider router={router} />
+//     </>
+//   );
+// }
+
+// export default App;
+
+import { HashRouter, Routes, Route, BrowserRouter } from "react-router";
+import Registration from "./pages/Registration";
 
 function App() {
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Routes>
+        <Route path="/registration" element={<Registration />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
